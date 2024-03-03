38ed124aa116aa8f490ae66504bf997e1716b988
Stephen Mascarenhas <stfcr7@gmail.com>
2024-03-03 22:40:44 +0530

    Create cortex_web.py

---
+++ chatgpt_web_clone/cortex_web.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are the smartest AI on earth"}]
+
+def CustomChatGPT(Input):
+    messages.append({"role": "user", "content": Input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "CORTEX")
+
+demo.launch(share=True)
